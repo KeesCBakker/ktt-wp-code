a1881da25aeb9725e3a014a9eee4c5f5a174d016
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2024-11-06 00:10:17 +0000

    Update version to 1.3.6

--- ktt-wp-code.php
+++ ktt-wp-code.php
@@ -8,7 +8,7 @@
  * Plugin Name:   KeesTalksTech Code
  * Plugin URI:    https://github.com/KeesCBakker/ktt-wp-code
  * Description:   A plugin to support Highlight.js and Mermaid
- * Version:       1.3.5
+ * Version:       1.3.6
  * Author:        Kees C. Bakker
  * Author URI:    https://keestalkstech.com/
  * License:       MIT
